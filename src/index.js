1db9bd3cbd4e3418cfdca99bffacb0226ca99998
jhonathanmusa <jonathansm891@gmail.com>
2020-10-03 18:24:39 -0500

    thirth commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
+app.use(require("./routes/index"));
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
