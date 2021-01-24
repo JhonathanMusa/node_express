fcfe8eda432d1230378174e2162ddcab82c47c47
Jhonathan <54297074+JhonathanMusa@users.noreply.github.com>
2021-01-24 14:46:19 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
+const PORT = proces.env.PORT || 3000
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(require("./routes/index"));
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(PORT, () => {
+  console.log("Server running on port ", PORT);
 });
